768843116265f71f3a2e70b16a25f1ed5099d1db
wwwbijay <link2bj@gmail.com>
2022-04-19 12:57:46 +0545

    amount update on rate and qty changes

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -5,31 +5,39 @@ import { countries } from '../constants/countries.contstant';
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  styleUrls: ['./invoice.component.css'],
 })
 export class InvoiceComponent implements OnInit {
-
   companyLogo: any;
   companyLogoPath: any;
   countryList: any;
 
+  subscription: any;
+
   invoiceInfoForm!: FormGroup;
-  constructor(private _fb: FormBuilder) { }
+  constructor(private _fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.invoiceInfoForm = this._fb.group({
-      items: this._fb.array([
-        {
-          itemDescription: ['', Validators.required],
-          qty: ['1', Validators.required],
-          rate: ['', Validators.required],
-        }
-      ]),
-    });
+    this.formInitialize();
   }
 
   get items() {
-    return this.invoiceInfoForm.controls["items"] as FormArray;
+    return this.invoiceInfoForm.controls['items'] as FormArray;
+  }
+
+  formInitialize() {
+    this.invoiceInfoForm = this._fb.group({
+      items: this._fb.array([
+        this._fb.group({
+          itemDescription: this._fb.control(null),
+          qty: this._fb.control(1),
+          rate: this._fb.control(1),
+          amount: this._fb.control(1),
+        }),
+      ]),
+    });
+
+    // this.invoiceInfoForm.controls['items'].setValue([]);
   }
 
   uploadLogo(e: any) {
@@ -39,22 +47,35 @@ export class InvoiceComponent implements OnInit {
       reader.readAsDataURL(e.target.files[0]);
       reader.onload = (event: any) => {
         this.companyLogoPath = event.target.result;
-      }
+      };
     }
   }
 
   addLineItem() {
     const itemLists = this.invoiceInfoForm.get('items') as FormArray;
-    itemLists.push(this._fb.group({
-      itemDescription: ['', Validators.required],
-      qty: ['1', Validators.required],
-      rate: ['', Validators.required],
-    }));
-
+    itemLists.push(
+      this._fb.group({
+        itemDescription: ['', Validators.required],
+        qty: ['1', Validators.required],
+        rate: ['1', Validators.required],
+        amount: ['1', Validators.required],
+      })
+    );
   }
 
-  deleteRow(index: any) {
+  deleteLineItem(index: any) {
     this.items.removeAt(index);
   }
 
+  updateAmount(index: any) {
+    let qty = 0;
+    let rate = 0;
+    let allItems = this.invoiceInfoForm.controls['items'] as FormArray;
+    let formGroup = allItems.at(index) as FormGroup;
+
+    qty = formGroup.get('qty')?.value;
+    rate = formGroup.get('rate')?.value;
+    formGroup.get('amount')?.patchValue(qty * rate)
+    //formGroup.get('amount').patchValue(111);
+  }
 }
