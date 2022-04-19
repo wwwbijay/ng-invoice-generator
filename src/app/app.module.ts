8b8775812cf94b88740a311b7dafc85cd22bc660
bj <link2bj@gmail.com>
2022-04-19 08:12:07 +0545

    working on formarray

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { InvoiceComponent } from './invoice/invoice.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { countries } from '../constants/countries.contstant';
 
 @Component({
@@ -8,41 +9,52 @@ import { countries } from '../constants/countries.contstant';
 })
 export class InvoiceComponent implements OnInit {
 
-  companyLogo:any;
-  companyLogoPath:any;
-  countryList:any;
+  companyLogo: any;
+  companyLogoPath: any;
+  countryList: any;
 
-  index=0;
-  itemQty:Array<any> = [1];
-  itemRate:Array<any> = [0];
-  itemTotal:Array<any> = [0];
-
-
-  itemLists:Array<any> = [ {value: 0} ];
-
-  constructor() { }
+  invoiceInfoForm!: FormGroup;
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.invoiceInfoForm = this._fb.group({
+      items: this._fb.array([
+        {
+          itemDescription: ['', Validators.required],
+          qty: ['1', Validators.required],
+          rate: ['', Validators.required],
+        }
+      ]),
+    });
   }
 
-  uploadLogo(e:any){
+  get items() {
+    return this.invoiceInfoForm.controls["items"] as FormArray;
+  }
+
+  uploadLogo(e: any) {
     if (e.target.files.length > 0) {
       this.companyLogo = e.target.files[0];
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
-      reader.onload= (event:any)=>{
-        this.companyLogoPath=event.target.result;
+      reader.onload = (event: any) => {
+        this.companyLogoPath = event.target.result;
       }
     }
   }
 
-  addLineItem(){
-    let len = this.itemLists.length;
-    this.itemLists.push({value:len});
+  addLineItem() {
+    const itemLists = this.invoiceInfoForm.get('items') as FormArray;
+    itemLists.push(this._fb.group({
+      itemDescription: ['', Validators.required],
+      qty: ['1', Validators.required],
+      rate: ['', Validators.required],
+    }));
+
   }
 
-  deleteRow(index:any){
-    this.itemLists.splice(index,1);
+  deleteRow(index: any) {
+    this.items.removeAt(index);
   }
 
 }
